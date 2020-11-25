e21d31477bffe29a55bf1cb35c440d8f1fef1097
Nico Martin <mail@nicomartin.ch>
2020-11-25 16:59:26 +0100

    clean up

--- src/apply.ts
+++ src/apply.ts
@@ -42,25 +42,8 @@ export const applyMetas = (
         `<${normalized.tag} ${attributes}>${normalized.content}</${normalized.tag}>`
       );
     } else {
-      console.log(key, normalized.tag, attributes);
-
       $('head').append(`<${normalized.tag} ${attributes}>`);
     }
-
-    /*
-
-    value = escapeHtml(value);
-    if (key === 'title') {
-      const $title = $('title');
-      $title.length
-        ? $title.text(value)
-        : $('head').append(`<title>${value}</title>`);
-    } else {
-      const $meta = $(`head meta[name="${key}"]`);
-      $meta.length
-        ? $meta.attr('content', value)
-        : $('head').append(`<meta name="${key}" content="${value}">`);
-    }*/
   });
   return $.html();
 };
